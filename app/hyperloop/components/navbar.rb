7cb572df055cbf0efa0649b5eb5d3e38644b6512
Ken Burgett <keburgett@gmail.com>
2017-10-04 16:05:23 -0700

    navbar.rb

--- app/hyperloop/components/navbar.rb
+++ app/hyperloop/components/navbar.rb
@@ -1,15 +1,12 @@
 #/app/hyperloop/components/navbar.rb
 
 class Navbar < Hyperloop::Component
+  
   render(NAV) do
     NAV(class: 'navbar navbar-default') do
       DIV(class: 'container-fluid') do
         PorterStatus {}
         WaterStatus {}
-        # if Rails.env.development?
-        #   EventGeneratorRadioButtons{}
-        #   ScaleSelect {}
-        # end
         TitleNav {}
         ValveButtons {}
       end
